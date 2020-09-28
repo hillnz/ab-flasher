36f8ddb848e5eca92ecb650c49bbf8effa40abe0
Jono Hill <jono@debian.wyvern>
2020-09-28 21:06:17 +1300

    wip

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker build -t ab . && \
+docker run --privileged -v /:/host ab \
+    -vv \
+    --host /host \
+    --hash-url http://localhost:8000/os.img.sha256 \
+    1.2.3 \
+    http://localhost:8000/boot.tar.gz \
+    sda2 \
+    http://localhost:8000/os.img.gz \
+    sda1,sda3
